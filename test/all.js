a0434c39fb63255dbefcc77cd0036dd82b461366
rgrannell1 <r.grannell2@gmail.com>
2014-07-01 11:23:30 +0100

    added generic checking function

--- test/all.js
+++ test/all.js
@@ -5,17 +5,19 @@ const is = require('../lib/is.js')
 
 
 
-
 console.assert(is.string(''))
 console.assert(is.string('string'))
+console.assert(is.a('string', 'string'))
 
 console.assert(is.number(-1))
 console.assert(is.number(0))
 console.assert(is.number(+1))
+console.assert(is.a('number', +1))
 
 console.assert(is.number(NaN))
 console.assert(is.number(+Infinity))
 console.assert(is.number(-Infinity))
+console.assert(is.a('number', +Infinity))
 
 console.assert(is.function(Math.max))
 
